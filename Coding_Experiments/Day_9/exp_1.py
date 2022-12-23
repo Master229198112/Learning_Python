acc61e3bf69563d9b7fd11ea84222ab0241fd5ae
Your Name <you@example.com>
2022-12-23 13:14:32 +0530

    Update on 23/12/2022

--- Coding_Experiments/Day_9/exp_1.py
+++ Coding_Experiments/Day_9/exp_1.py
@@ -3,19 +3,19 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if 'add' in user_action or 'new' in user_action:
         todo = user_action[4:]
 
-        with open('../File/todos.txt', 'r') as file:
+        with open(r'E:\Learning\File\todos.txt', 'r') as file:
             todos = file.readlines()
 
         todos.append(todo)
 
-        with open('../File/todos.txt', 'w') as file:
+        with open(r'E:\Learning\File\todos.txt', 'w') as file:
             file.writelines(todos)
 
     elif 'show' in user_action:
-        with open('../File/todos.txt', 'r') as file:
+        with open(r'E:\Learning\File\todos.txt', 'r') as file:
             todos = file.readlines()
 
         for index, item in enumerate(todos):
@@ -27,25 +27,25 @@ while True:
         print(number)
         number = number - 1
 
-        with open('../File/todos.txt', 'r') as file:
+        with open(r'E:\Learning\File\todos.txt', 'r') as file:
             todos = file.readlines()
 
         new_todo = input("Enter new todo: ")
         todos[number] = new_todo + '\n'
 
-        with open('../File/todos.txt', 'w') as file:
+        with open(r'E:\Learning\File\todos.txt', 'w') as file:
             file.writelines(todos)
 
     elif 'complete' in user_action:
         number = int(user_action[9:])
 
-        with open('../File/todos.txt', 'r') as file:
+        with open(r'E:\Learning\File\todos.txt', 'r') as file:
             todos = file.readlines()
         index = number - 1
         todo_to_remove = todos[index].strip('\n')
         todos.pop(index)
 
-        with open('../File/todos.txt', 'w') as file:
+        with open(r'E:\Learning\File\todos.txt', 'w') as file:
             file.writelines(todos)
 
         message = f"Todo {todo_to_remove} was removed from the list."
